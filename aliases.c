b7f6f7d6e20363fc3f62828bf4e470a36ace5c92
Mohamed8221 <s21511383eg@std.aou.edu.eg>
2024-01-02 06:24:05 +0200

    update

--- aliases.c
+++ aliases.c
@@ -1,4 +1,5 @@
 #include "shell.h"
+
 /**
 * handle_alias - Handles the alias command
 * @args: Arguments
@@ -13,34 +14,48 @@ alias_t *new_alias, *temp;
 if (args[1] == NULL)
 {
 /* If no arguments, print all aliases */
-temp = *alias_list;
-while (temp != NULL)
+for (temp = *alias_list; temp != NULL; temp = temp->next)
 {
 printf("%s='%s'\n", temp->name, temp->value);
-temp = temp->next;
 }
 }
-else if (strchr(args[1], '=') != NULL)
+else
 {
-/* If argument contains '=', add new alias */
+char *name = strtok(args[1], "=");
+char *value = strtok(NULL, "=");
+
+if (value != NULL)
+{
+/* If argument contains '=', add new alias or update existing one */
+for (temp = *alias_list; temp != NULL; temp = temp->next)
+{
+if (strcmp(temp->name, name) == 0)
+{
+/* Update existing alias */
+free(temp->value);
+temp->value = strdup(value);
+return (1);
+}
+}
+
+/* Add new alias */
 new_alias = malloc(sizeof(alias_t));
-new_alias->name = strtok(args[1], "=");
-new_alias->value = strtok(NULL, "=");
+new_alias->name = strdup(name);
+new_alias->value = strdup(value);
 new_alias->next = *alias_list;
 *alias_list = new_alias;
 }
 else
 {
 /* If argument doesn't contain '=', print the value of the alias */
-temp = *alias_list;
-while (temp != NULL)
+for (temp = *alias_list; temp != NULL; temp = temp->next)
 {
-if (strcmp(temp->name, args[1]) == 0)
+if (strcmp(temp->name, name) == 0)
 {
 printf("%s='%s'\n", temp->name, temp->value);
 break;
 }
-temp = temp->next;
+}
 }
 }
 return (1);
